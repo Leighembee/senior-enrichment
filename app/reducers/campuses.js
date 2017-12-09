d7ff3062dbff7b19a3985ffa0cff3db4b99f47d4
Leigh Blechman <leigh.blechman@gmail.com>
2017-12-09 15:19:57 -0500

    finished setting up campus reducer

--- app/reducers/campuses.js
+++ app/reducers/campuses.js
@@ -1,6 +1,10 @@
 import axios from 'axios'
 
-const GET_CAMPUSES = "GET_STUDENTS"
+const GET_CAMPUSES = 'GET_CAMPUSES'
+const CREATE_CAMPUS = 'CREATE_CAMPUS'
+const UPDATE_CAMPUS = 'UPDATE_CAMPUS'
+const CHANGE_STUDENT = 'CHANGE_STUDENT'
+const DELETE_CAMPUS = 'DELETE_CAMPUS'
 
 
 //action creators
@@ -11,25 +15,94 @@ export function getCampuses(campuses) {
   }
 }
 
+function createCampus(campus) {
+  return {
+    type: CREATE_CAMPUS,
+    campus: campus
+  }
+}
 
-// // thunks
+function updateCampus(campus) {
+  return {
+    type: UPDATE_CAMPUS,
+    campus: campus
+  }
+}
+
+function changeStudent(student) {
+  return {
+    type: CHANGE_STUDENT,
+    student: student
+  }
+}
+
+function deleteCampus() {
+  return {
+    type: DELETE_CAMPUS,
+    campus: campus
+  }
+}
+
+
+
+ // thunks
 
 export const fetchCampuses = () => {
   return dispatch => {
     axios.get('/api/campuses')
-      .then(res => res.data)
-    .then(campusess => dispatch(getCampuses(campuses)))
+    .then(res => res.data)
+    .then(campuses => dispatch(getCampuses(campuses)))
+    .catch(err => console.error(err))   // add error messages later
   }
 }
 
+export const createNewCampus = campus => {
+  return dispatch => {
+    axios.post('/api/campuses', campus)
+    .then(res => res.data)
+    .then(data => dispatch(createCampus(data)))
+    .catch(err => console.error(err))
+  }
+}
+
+export const updateExistingCampus = campus => {
+  return dispatch => {
+    dispatch(updatecampus(campus))
+    axios.put(`/api/campuses/${campus.id}`, campus)
+    .catch(err => console.error(err => console.error(err)))
+  }
+}
+
+
+export const deleteExistingCampus = campus => {
+  return dispatch => {
+    dispatch(deletecampus(campus))
+    axios.delete(`/api/campuses/${campus.id}`)
+    .catch(err => console.error(err))
+  }
+}
+
+
 //reducer
 
-export default function campusReducer(state = [], action) {
+export default function campusesReducer(state = [], action) {
 
   switch (action.type) {
-
     case GET_CAMPUSES:
       return action.campuses
+
+    case CREATE_CAMPUS:
+      return [...campuses, action.campus]
+
+    case UPDATE_CAMPUS:
+      return campusesReducer.map(campus => (
+        action.campus.id === campus.id ? Object.assign({}, campus, action.campus) : campus
+      ))
+
+    case DELETE_CAMPUS:
+      return campuses.filter(campus =>
+        campus.id !== action.campus.id
+      )
 
     default:
       return state
