a289b46e35391a44d952e63017c9dc6caca40374
Leigh Blechman <leigh.blechman@gmail.com>
2017-12-09 15:03:42 -0500

    finished student reducers

--- app/reducers/students.js
+++ app/reducers/students.js
@@ -42,25 +42,67 @@ function deleteStudent() {
     student: student
   }
 }
-// // thunks
 
+
+
+ // thunks
 
 export const fetchStudents = () => {
   return dispatch => {
     axios.get('/api/students')
-      .then(res => res.data)
+    .then(res => res.data)
     .then(students => dispatch(getStudents(students)))
+    .catch(err => console.error(err))   // add error messages later
   }
 }
+
+export const createNewStudent = student => {
+  return dispatch => {
+    axios.post('/api/students', student)
+    .then(res => res.data)
+    .then(data => dispatch(createStudent(data)))
+    .catch(err => console.error(err))
+  }
+}
+
+export const updateExistingStudent = student => {
+  return dispatch => {
+    dispatch(updateStudent(student))
+    axios.put(`/api/students/${student.id}`, student)
+    .catch(err => console.error(err => console.error(err)))
+  }
+}
+
+
+export const deleteExistingStudent = student => {
+  return dispatch => {
+    dispatch(deleteStudent(student))
+    axios.delete(`/api/students/${student.id}`)
+    .catch(err => console.error(err))
+  }
+}
+
 
 //reducer
 
 export default function studentsReducer(state = [], action) {
 
   switch (action.type) {
-
     case GET_STUDENTS:
       return action.students
+
+    case CREATE_STUDENT:
+      return [...students, action.student]
+
+    case UPDATE_STUDENT:
+      return studentsReducer.map(student => (
+        action.student.id === student.id ? Object.assign({}, student, action.student) : student
+      ))
+
+    case DELETE_STUDENT:
+      return students.filter(student =>
+        student.id !== action.student.id
+      )
 
     default:
       return state
