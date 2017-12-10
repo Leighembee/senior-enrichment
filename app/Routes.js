027cb626dfbd50060c6a492e9e0e32031ee5f741
Leigh Blechman <leigh.blechman@gmail.com>
2017-12-10 04:02:01 -0500

    fixed campus rendering issue

--- app/Routes.js
+++ app/Routes.js
@@ -27,8 +27,8 @@ class Routes extends Component {
         <Root>
         <Switch>
           <Route exact path="/" component={Home} />
-          {/* <Route exact path="/campuses" component={Campuses} /> */}
-          {/* <Route path="/campuses/:campusId" component={SingleCampus} /> */}
+          <Route exact path="/campuses" component={Campuses} />
+         <Route path="/campuses/:campusId" component={SingleCampus} />
           <Route exact path="/students" component={Students} />
           <Route path="/students/:studentId" component={SingleStudent}/>
           </Switch>
